76fd30373dabab605d13bc077fae14308c9388fc
Marifuhr <mariarosafuhr@gmail.com>
2023-11-28 17:28:21 -0300

    colocando boton de whatsapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import './App.css'
-import NavBar from './Components/NavBar'
+import './App.css';
+import NavBar from './Components/NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Servicios from './Components/Servicios';
 import Inicio from './Components/Inicio';
 import Nosotros from './Components/Nosotros';
 import Contacto from './Components/Contacto';
-
-
+import whatsappLogo from './assets/logo_what.png';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [showButton, setShowButton] = useState();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowButton(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const openWhatsAppChat = () => {
+    var phoneNumber = '+5493492671315';
+    var url = 'https://wa.me/' + phoneNumber;
+    window.open(url);
+  };
+
   return (
     <>
       <Router>
@@ -20,8 +36,13 @@ function App() {
           <Route path="/contacto" element={<Contacto />} />
         </Routes>
       </Router>
+      {showButton && (
+        <button id="openWhatsAppButton" className="whatsapp-button" onClick={openWhatsAppChat}>
+          <img src={whatsappLogo} alt="WhatsApp" />
+        </button>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
