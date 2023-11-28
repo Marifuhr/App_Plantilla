11a2992148c9b62e09b499bc18d3a85d043a80a5
Marifuhr <mariarosafuhr@gmail.com>
2023-11-27 22:04:32 -0300

    creando footer

--- src/Components/Contacto.jsx
+++ src/Components/Contacto.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Footer from './Footer'
 
 export default function Contacto() {
   return (
     <div>
       <h1>Contacto</h1>
+      <Footer />
     </div>
   )
 }
--- src/Components/Inicio.jsx
+++ src/Components/Inicio.jsx
@@ -4,11 +4,8 @@ import camping from '../assets/camping_jbc.jpeg';
 export default function Inicio() {
   return (
     <div style={{ display: 'flex', alignItems: 'center' }}>
-      <img src={camping} alt="not found" style={{ width: '50%', marginLeft: '-10px', marginTop: '200px' }} />
-      <div style={{ marginLeft: '20px' }}>
-        <h1>Inicio</h1>
-        <p>Tu texto aquí</p>
-      </div>
+      
+    
     </div>
   );
 }
